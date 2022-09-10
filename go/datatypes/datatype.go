bcef7f913303763d222cb5baa3ab3a68051c65e0
Shanith K K <shanith.kk@hugobyte.com>
2022-09-10 21:35:49 +0530

    Added integer datatypes

---
+++ go/datatypes/datatype.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var typeint8 int8
+	var typeint16 int16
+	var typeint32 int32
+	var typeint64 int64
+	fmt.Println(typeint8, typeint16, typeint32, typeint64)
+
+	var typeuint8 uint8
+	var typeuint16 uint16
+	var typeuint32 uint32
+	var typeuint64 uint64
+	fmt.Println(typeuint8, typeuint16, typeuint32, typeuint64)
+
+	var boolean bool
+	var float float32
+	fmt.Println(boolean, float)
+}
