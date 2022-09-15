0887fbccaf50517ab93db08d631360cea9480206
Shanith K K <shanith.kk@hugobyte.com>
2022-09-15 10:25:17 +0530

    if condition

---
+++ go/condition&loop/if.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Condition and Looping")
+
+	x := 70
+	if x == 50 {
+		fmt.Println("Number is 50")
+	} else if x > 50 {
+		fmt.Println("Number is greater than 50")
+	} else {
+		fmt.Println("Number if less than 50")
+	}
+
+}
