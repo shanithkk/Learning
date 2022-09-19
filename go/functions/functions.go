1a7710214fba15fe36a5aa987c3acbe274e1886a
Shanith K K <shanith.kk@hugobyte.com>
2022-09-19 11:24:08 +0530

    variadic function

--- go/functions/functions.go
+++ go/functions/functions.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 var (
 	area = func(l int, b int) int {
@@ -23,6 +26,9 @@ func main() {
 	fmt.Println(squareSum(5)(6)(7))
 
 	fmt.Println(square_Sum(5)(6)(7))
+
+	variadicExample(1, "red", true, 10.5, []string{"foo", "bar", "baz"},
+		map[string]int{"apple": 23, "tomato": 13})
 }
 
 func squareSum(x int) func(int) func(int) int {
@@ -41,5 +47,11 @@ func square_Sum(x int) Second {
 		return func(z int) int {
 			return x*x + y*y + z*z
 		}
+	}
+}
+
+func variadicExample(i ...interface{}) {
+	for _, v := range i {
+		fmt.Println(v, "--", reflect.ValueOf(v).Kind())
 	}
 }
