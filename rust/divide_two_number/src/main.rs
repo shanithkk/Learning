0480a9ece5ffcb9bd5dd2b9029554eb69a269b32
Shanith K K <shanith.kk@hugobyte.com>
2022-09-20 11:33:00 +0530

    removed comments

--- rust/divide_two_number/src/main.rs
+++ rust/divide_two_number/src/main.rs
@@ -7,14 +7,9 @@ fn main() {
 }
 
 pub fn divide(dividend: i32, divisor: i32) -> i32 {
-    // match  Wrapping(dividend)/Wrapping(divisor) {
-    //     x => x.0,
-    //     _ => i32::MAX,
-    // } 
     if dividend == -2147483648 && divisor == -1{
         return 2147483647;
     }
-    //a = dividend/divisor;
     let a =Wrapping(dividend)/Wrapping(divisor);
     a.0
 }
