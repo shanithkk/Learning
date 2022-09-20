30e4f9be0bd2601696f54cfb9e8838ba4417bc1c
Shanith K K <shanith.kk@hugobyte.com>
2022-09-20 11:02:47 +0530

    update on config

--- rust/remove_element/src/main.rs
+++ rust/remove_element/src/main.rs
@@ -1,11 +1,9 @@
 fn main() {
-    println!("Hello, world!");
     let mut res = vec![2,4,5,3,2,7,8];
     let f =remove_element(&mut res, 2);
 }
 
 pub fn remove_element(nums: &mut Vec<i32>, val: i32) -> i32 {
-    
     nums.retain(|&x| x!= val);
     nums.len() as i32
 }
