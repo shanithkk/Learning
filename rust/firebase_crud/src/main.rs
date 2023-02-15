5f1231a55981db7a367aa657616ecd9c9b33514c
Shanith K K <shanith.kk@hugobyte.com>
2023-02-15 17:16:00 +0530

    firebase example

---
+++ rust/firebase_crud/src/main.rs
@@ -0,0 +1,29 @@
+use std::collections::HashMap;
+
+use firebase_rs::*;
+// use serde_json;
+use serde::{Deserialize,Serialize};
+
+#[tokio::main]
+async fn main() {
+    let firebase = Firebase::new("<firebase url>").unwrap();
+
+    let user = User{
+        username: "testuser".to_string(),
+        password: "testpassword".to_string(),
+        fullname: "testfullname".to_string(),
+    };
+    let fire = firebase.at("users");
+    let s = fire.set(&user).await;
+    println!("{:?}", s);
+
+    let get = fire.get::<HashMap<String, User>>().await;
+    println!("{:?}", get);
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User{
+    username: String,
+    password: String,
+    fullname: String,
+}
